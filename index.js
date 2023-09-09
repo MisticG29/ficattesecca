17f6da91fce141360c38e6cb3dfd23abe9b469c8
pellinuz <78985892+pellinuz@users.noreply.github.com>
2023-09-09 13:14:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const inputFolder = './public/tags';
 // Percorso del file JSON di output
 const outputFile = './tags.json';
 
-// Funzione ricorsiva per ottenere i file e le cartelle nidificate
 function getNestedItems(folderPath) {
     const dirents = fs.readdirSync(folderPath, { withFileTypes: true });
     const folders = dirents.filter(dirent => dirent.isDirectory());
@@ -20,13 +19,13 @@ function getNestedItems(folderPath) {
 
     folders.forEach(folder => {
         const nestedFolderPath = path.join(folderPath, folder.name);
-        const nestedItemsInFolder = getNestedItems(nestedFolderPath); // Chiamata ricorsiva per ottenere gli elementi nidificati
+        const nestedItemsInFolder = getNestedItems(nestedFolderPath);
 
         const folderItem = {
             name: folder.name,
             color: "#2178f3",
             tags: nestedItemsInFolder,
-            files: [] // Aggiungi una proprietà 'files' inizialmente vuota
+            files: [
         };
 
         nestedItems.push(folderItem);
@@ -46,33 +45,27 @@ function getNestedItems(folderPath) {
 
 const tags = getNestedItems(inputFolder);
 
-// Salva i tag nel file JSON
 fs.writeFileSync(outputFile, JSON.stringify(tags, null, 2));
 
 app.use(express.static('public'));
 
-// Configurazione della visualizzazione dei tag
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
 app.get('/tags', (req, res) => {
-    // Leggi il file JSON dei tag
+
     const tagsData = fs.readFileSync(outputFile, 'utf8');
     const tags = JSON.parse(tagsData);
 
-    // Renderizza la pagina dei tag utilizzando il template ejs
     res.render('tags', { tags });
 });
 
-// Rotta per visualizzare i media
 app.get('/media', (req, res) => {
     res.render('media', { tags });
 });
 
-
-// Avvia il server
-const port = 2317;
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server in ascolto sulla porta ${port}`);
+    console.log(`Server port: ${port}`);
 });
